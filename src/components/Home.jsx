7dfc9f02432989b8f0013e263afd75bf3361a7a4
Tanveer Ahmed <tanveerahmed85820@gmail.com>
2024-10-09 11:16:04 +0530

    modified Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,23 +1,65 @@
 import React from "react";
 import { useState } from "react";
+import { useDispatch } from "react-redux";
 import { useSearchParams } from "react-router-dom";
+import { addToPastes } from "../redux/pasteSlice";
+import { updateToPastes } from "../redux/pasteSlice";
 
 const Home = () => {
   const [title, setTitle] = useState("");
   const [value, setValue] = useState("");
   const [searchParams, setSearchParams] = useSearchParams();
   const pasteId = searchParams.get("pasteId");
-  return (
-    <div className="flex flex-row gap-7">
-      <input
-        className="p-2 rounded-2xl mt-2 "
-        type="text"
-        placeholder="Enter title here"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-      />
+  const dispatch = useDispatch();
 
-      <button>{pasteId ? "Updated My Button" : "Create My Button"}</button>
+  function createPaste() {
+    const paste = {
+      title: title,
+      content: value,
+      _id: pasteId || Date.now().toString(36),
+      createdAt: new Date().toISOString(),
+    };
+
+    if (pasteId) {
+      //update
+      dispatch(updateToPastes(paste));
+    } else {
+      //create
+      dispatch(addToPastes(paste));
+    }
+
+    //after creation and updation
+
+    setTitle("");
+    setValue("");
+    setSearchParams({});
+  }
+
+  return (
+    <div>
+      <div className="flex flex-row gap-7 place-content-between">
+        <input
+          className="p-1 rounded-2xl mt-2 w-[65%] pl-4"
+          type="text"
+          placeholder="Enter title here"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <button onClick={createPaste} className="p-2 rounded-2xl mt-2 ">
+          {pasteId ? "Updated My Button" : "Create My Button"}
+        </button>
+      </div>
+
+      <div className="mt-8">
+        <textarea
+          className="rounded-2xl mt-4, min-w-[500px] p-4"
+          value={value}
+          placeholder="Enter Content Here"
+          onChange={(e) => setValue(e.target.value)}
+          rows={20}
+        />
+      </div>
     </div>
   );
 };
--- src/components/Paste.jsx
+++ src/components/Paste.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const Paste = () => {
-  return <div>View of Paste</div>;
+  const pastes = useSelector((state) => state.paste.pastes);
+
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const dispatch = useDispatch();
+
+  const filteredData = pastes.filter((paste) =>
+    paste.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div>
+      <input
+        className="p-2 rounded-2xl min-w-[600px] mt-5"
+        type="search"
+        placeholder="Search here"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+    </div>
+  );
 };
 
 export default Paste;
